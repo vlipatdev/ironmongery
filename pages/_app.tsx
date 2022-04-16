711b6717ae1ad714c3ad51a625a04a91a70b8d9b
Val Lipat <vlipat.dev@gmail.com>
2022-04-16 20:46:22 +0800

    add nextjs progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import NextNProgress from 'nextjs-progressbar'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<NextNProgress color="#EDA750" options={{ showSpinner: false }} />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
