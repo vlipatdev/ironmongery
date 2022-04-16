1dcbc4c7c1116d7ba02a9e02fe3d6eee2bac6025
Val Lipat <vlipat.dev@gmail.com>
2022-04-16 15:41:32 +0800

    fix logo positioning

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
 	return (
 		<header className="flex justify-center items-center h-[150px] bg-dark-gray relative">
 			<Link href="/" passHref>
-				<a className="h-full w-44 absolute top-0 left-8">
+				<a className="h-12 w-44 absolute top-1/2 -translate-y-1/2 left-8">
 					<Image src={ironMongeryLogo} alt="IronMongery Logo" layout="fill" objectFit="contain" />
 				</a>
 			</Link>
